73eecd898b3142a8b5ef9025affe6ff35adab62d
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2016-02-01 14:41:06 +0100

    Add id field in node models

--- src/main/java/name/matco/simcity/model/Building.java
+++ src/main/java/name/matco/simcity/model/Building.java
@@ -2,5 +2,6 @@ package name.matco.simcity.model;
 
 public class Building {
 
+	public String id;
 	public String name;
 }
--- src/main/java/name/matco/simcity/model/Ingredient.java
+++ src/main/java/name/matco/simcity/model/Ingredient.java
@@ -2,6 +2,7 @@ package name.matco.simcity.model;
 
 public class Ingredient {
 
+	public String id;
 	public String name;
 	public int time;
 }
