9bfaac9de54ab3b1801cb46ad90a5fff848a3e1b
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2016-02-06 21:28:46 +0100

    Improve model

--- src/main/java/name/matco/simcity/model/Building.java
+++ src/main/java/name/matco/simcity/model/Building.java
@@ -1,5 +1,9 @@
 package name.matco.simcity.model;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
+
+@JsonInclude(value = Include.NON_NULL)
 public class Building {
 
 	public String id;
---
+++ src/main/java/name/matco/simcity/model/Dependency.java
@@ -0,0 +1,6 @@
+package name.matco.simcity.model;
+
+public class Dependency extends Ingredient {
+
+	public Long quantity;
+}
