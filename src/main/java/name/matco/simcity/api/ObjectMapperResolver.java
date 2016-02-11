189e5288928ad40192bd1594351746ea5ae7db7f
Matthieu Corageoud <matthieu.corageoud@gmail.com>
2016-02-11 18:17:25 +0100

    Improve indentation of JSON content from REST API

--- src/main/java/name/matco/simcity/api/ObjectMapperResolver.java
+++ src/main/java/name/matco/simcity/api/ObjectMapperResolver.java
@@ -1,10 +1,19 @@
 package name.matco.simcity.api;
 
+import java.io.IOException;
+
+import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.ext.ContextResolver;
 import javax.ws.rs.ext.Provider;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.jaxrs.cfg.EndpointConfigBase;
+import com.fasterxml.jackson.jaxrs.cfg.ObjectWriterInjector;
+import com.fasterxml.jackson.jaxrs.cfg.ObjectWriterModifier;
 
 @Provider
 public class ObjectMapperResolver implements ContextResolver<ObjectMapper> {
@@ -13,7 +22,15 @@ public class ObjectMapperResolver implements ContextResolver<ObjectMapper> {
 
 	public ObjectMapperResolver() {
 		mapper = new ObjectMapper();
-		mapper.enable(SerializationFeature.INDENT_OUTPUT);
+		ObjectWriterInjector.set(new ObjectWriterModifier() {
+			@Override
+			public ObjectWriter modify(final EndpointConfigBase<?> endpoint, final MultivaluedMap<String, Object> responseHeaders, final Object valueToWrite, final ObjectWriter w, final JsonGenerator g) throws IOException {
+				final DefaultPrettyPrinter pp = new DefaultPrettyPrinter();
+				pp.indentObjectsWith(new DefaultIndenter("\t", "\n"));
+				g.setPrettyPrinter(pp);
+				return w;
+			}
+		});
 	}
 
 	@Override
